16b6fdf9d38d2a6b2ccbd947b9cfa33779a57616
TetoTigas <tp.costa@unesp.br>
2025-07-24 21:02:53 -0300

    Hlov

--- aula10/exaula1.c
+++ aula10/exaula1.c
@@ -2,25 +2,37 @@
 #include <stdlib.h>
 
 void fiveVal(){
-    int nv[5], big, small;
+    int nv[5];
     for(int i = 0; i < 5; i++){
         printf("Enter 5 values: ");
         scanf("%d", &nv[i]);
         printf("| %d | \n", nv[i]);
     }
 
-    for(int i = 0; i < 5; i++){
-        if(nv[i] > big){
-            nv[i] == big;
-            printf("%d", big);
-        }else if( small > nv[i]){
-            nv[i] == small;
-        }
-    }
-    printf("Biggest: %d\n", big);
-    printf("Smallest: %d\n", small);
+    //sorting the array using insertion sort
+    int i= 0;
+    int j= 1;
+    int aux = 0;
+
+    while (j < 5)
+    {
+    aux = nv[j];
+    i = j - 1;
+     while ((i >= 0) && (nv[i] > aux))
+     {
+     nv[i + 1] = nv[i];
+     i = i - 1;
+     }
+     nv[i + 1] = aux;
+     j = j + 1;
+     }
+     //end of sorting
+
+
+    printf("biggest: %d\n", nv[4]);
+    printf("smallest: %d\n", nv[0]);
 }
 int main(){
-    fiveVal();
+    fiveVal();  
     return 0;
 }
--- aula5/exaula3.c
+++ aula5/exaula3.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
-  int list[4], x;
-    printf("Coloque as 5 idades \n");
-    scanf("%d %d %d %d %d",&list[0], &list[1], &list[2], &list[3], &list[4]);
-    for(int i = 0; i < 5; i++ ){
-        if (list[i]>18){
-            x++;
+  int N, cnt = 0;
+    printf("Digite N \n");
+    scanf("%d",&N);
+    for(int i = 1; i <= N; i++ ){
+        for(int j = 1; j <= i; j++){
+            if(i % j == 0){
+                cnt++;
+            }
         }
-        printf("%d || %d || %d \n", x, list[i], i);
+        if(cnt <= 2){
+            printf("%d é primo \n", i); 
+        }
+        cnt = 0; // Reset count for the next number
     }
-    printf("%d pessoas são maiores de idade \n", x);
 }
